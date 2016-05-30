020be9c48486ce2fdb0d30028b1801e3258807c5
didjrc <didjrc@nus.edu.sg>
2016-05-30 15:29:58 +0800

    Initial Commit

---
+++ Count Logger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Count Logger
+//
+//  Created by Jonathan Chinen on 30/5/16.
+//  Copyright © 2016 Jonathan Chinen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
